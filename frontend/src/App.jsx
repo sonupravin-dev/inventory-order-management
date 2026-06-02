315f87bb6065e67cd4d601eb206fc26098725e63
Pravin Karampuri <pravinkarampuri211981@gmail.com>
2026-06-02 11:26:25 +0000

    Fix CORS middleware

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const API = "https://inventory-order-management-1-hhhm.onrender.com";
+  const API = "https://inventory-order-management-nhmh.onrender.com/";
 
   // PRODUCTS
   const [products, setProducts] = useState([]);
